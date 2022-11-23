21865e0bb044a311b9e0a88d71f3a3060de1f48f
mikeholford <mike@holfy.io>
2022-11-23 14:30:38 +0000

    FEAT: Soundcloud embed remove

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// const audio = document.querySelector("#audio");
+const audio = document.querySelector("#audio");
 
-// document.addEventListener('click', play);
+document.addEventListener('click', play);
 
-// function play() {
-//   if (audio.paused) {
-//     audio.play();
-//     play.innerHTML = "PAUSE";
-//   } else {
-//     audio.pause();
-//     play.innerHTML = "PLAY";
-//   }
-// }
+function play() {
+  if (audio.paused) {
+    audio.play();
+    play.innerHTML = "PAUSE";
+  } else {
+    audio.pause();
+    play.innerHTML = "PLAY";
+  }
+}
