bf8948cdbc00b886db8d2b8796df19e904d8810a
mikeholford <mike@holfy.io>
2022-11-23 14:26:53 +0000

    FEAT: Soundcloud embed

---
+++ main.js
@@ -0,0 +1,13 @@
+// const audio = document.querySelector("#audio");
+
+// document.addEventListener('click', play);
+
+// function play() {
+//   if (audio.paused) {
+//     audio.play();
+//     play.innerHTML = "PAUSE";
+//   } else {
+//     audio.pause();
+//     play.innerHTML = "PLAY";
+//   }
+// }
